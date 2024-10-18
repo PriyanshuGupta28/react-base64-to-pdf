b63b8b00dbb1888c97e3a06872e1bb675608b6a9
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-18 22:58:48 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as PDFInfo } from "./components/PDFInfo";
+export { default as PDFPreview } from "./components/PDFPreview";
+export { default as PDFDownloadButton } from "./components/PDFDownloadButton";
