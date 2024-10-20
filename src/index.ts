f184ffcad8a42d7d11d472012d433a0e380b40df
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-20 15:10:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as PDFInfo } from "./components/PDFInfo";
 export { default as PDFPreview } from "./components/PDFPreview";
 export { default as PDFDownloadButton } from "./components/PDFDownloadButton";
+
+export * from "./types";
