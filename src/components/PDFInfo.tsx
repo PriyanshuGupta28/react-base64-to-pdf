626b79379203749b47e0e46ae0097ecb12c3d553
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-10-20 15:10:45 +0530

    Update PDFInfo.tsx

--- src/components/PDFInfo.tsx
+++ src/components/PDFInfo.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-
-interface PDFInfoProps {
-  base64: string;
-}
+import { PDFInfoProps } from "../types";
 
 const PDFInfo: React.FC<PDFInfoProps> = ({ base64 }) => {
   const [fileSize, setFileSize] = useState<number | null>(null);
